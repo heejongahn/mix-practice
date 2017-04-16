389d61d9d325fd60c36406790113aa33e3bad2d5
Heejong Ahn <heejongahn@gmail.com>
2017-04-16 15:54:29 +0900

    Add KV.Bucket and corresponding test

---
+++ lib/kv/bucket.ex
@@ -0,0 +1,22 @@
+defmodule KV.Bucket do
+  @doc """
+  Starts a new bucket.
+  """
+  def start_link do
+    Agent.start_link(fn -> %{} end)
+  end
+
+  @doc """
+  Gets a value from the `bucket` by `key`.
+  """
+  def get(bucket, key) do
+    Agent.get(bucket, fn b -> Map.get(b, key) end)
+  end
+
+  @doc """
+  Puts the `value` for the given `key` in the `bucket`.
+  """
+  def put(bucket, key, value) do
+    Agent.update(bucket, fn b -> Map.put(b, key, value) end)
+  end
+end
